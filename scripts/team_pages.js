10edb5af64bb8c8d78b4036260c636a6713047bf
deloittemagicleap <deloittemagicleap@gmail.com>
2024-07-18 18:58:48 +0530

    First Commit

---
+++ scripts/team_pages.js
@@ -0,0 +1,152 @@
+// /* <script> */
+// const img = document.querySelector("img");
+// const icons = document.querySelector(".icons");
+// img.onclick = function () {
+//     this.classList.toggle("active");
+//     icons.classList.toggle("active");
+// }
+// // </script>
+
+/* <script> */ 
+function scroll_to_end() {
+    window.scrollTo(0, document.body.scrollHeight);
+}
+// </script>
+
+/* <script> */ 
+$(".close-btn, .bg-overlay").click(function () {
+    $(".custom-model-main").removeClass('model-open');
+});
+
+// Core Data
+$(".pop-abinash").on('click', function () {
+    $(".abinash").addClass('model-open');
+});
+
+$(".pop-piyush").on('click', function () {
+    $(".piyush").addClass('model-open');
+});
+
+$(".pop-biswadip").on('click', function () {
+    $(".biswadip").addClass('model-open');
+});
+
+// D2O
+$(".pop-shikha").on('click', function () {
+    $(".shikha").addClass('model-open');
+});
+
+$(".pop-vinod").on('click', function () {
+    $(".vinod").addClass('model-open');
+});
+
+$(".pop-dnyaneshwar").on('click', function () {
+    $(".dnyaneshwar").addClass('model-open');
+});
+
+// A2R
+$(".pop-ragavendran").on('click', function () {
+    $(".ragavendran").addClass('model-open');
+});
+
+$(".pop-ranjith").on('click', function () {
+    $(".ranjith").addClass('model-open');
+});
+
+$(".pop-ayesha").on('click', function () {
+    $(".ayesha").addClass('model-open');
+});
+
+$(".pop-janaki").on('click', function () {
+    $(".janaki").addClass('model-open');
+});
+
+// P2R
+$(".pop-srinivas").on('click', function () {
+    $(".srinivas").addClass('model-open');
+});
+
+$(".pop-aparna").on('click', function () {
+    $(".aparna").addClass('model-open');
+});
+
+$(".pop-rishabh").on('click', function () {
+    $(".rishabh").addClass('model-open');
+});
+
+$(".pop-shreya").on('click', function () {
+    $(".shreya").addClass('model-open');
+});
+
+$(".pop-mahesh").on('click', function () {
+    $(".mahesh").addClass('model-open');
+});
+
+$(".pop-devashish").on('click', function () {
+    $(".devashish").addClass('model-open');
+});
+
+$(".pop-animeri").on('click', function () {
+    $(".animeri").addClass('model-open');
+});
+
+// Core Development
+$(".pop-divya").on('click', function () {
+    $(".divya").addClass('model-open');
+});
+
+$(".pop-satabdi").on('click', function () {
+    $(".satabdi").addClass('model-open');
+});
+
+$(".pop-mohit").on('click', function () {
+    $(".mohit").addClass('model-open');
+});
+
+$(".pop-faisal").on('click', function () {
+    $(".faisal").addClass('model-open');
+});
+
+$(".pop-ajit").on('click', function () {
+    $(".ajit").addClass('model-open');
+});
+
+$(".pop-abraham").on('click', function () {
+    $(".abraham").addClass('model-open');
+});
+
+// Core Platform
+$(".pop-palash").on('click', function () {
+    $(".palash").addClass('model-open');
+});
+
+$(".pop-swati").on('click', function () {
+    $(".swati").addClass('model-open');
+});
+
+$(".pop-jetangi").on('click', function () {
+    $(".jetangi").addClass('model-open');
+});
+
+// L2C
+$(".pop-rabi").on('click', function () {
+    $(".rabi").addClass('model-open');
+});
+
+$(".pop-simon").on('click', function () {
+    $(".simon").addClass('model-open');
+});
+
+$(".pop-meenakshi").on('click', function () {
+    $(".meenakshi").addClass('model-open');
+});
+
+$(".pop-vivek").on('click', function () {
+    $(".vivek").addClass('model-open');
+});
+
+$(".pop-priyanka").on('click', function () {
+    $(".priyanka").addClass('model-open');
+});
+
+// </script>
